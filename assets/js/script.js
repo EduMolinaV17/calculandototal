c2a1961b2bbb3637630af0ad1fd5834ec7359654
Eduardo Molina <eduardo.molina.me@hotmail.com>
2023-11-10 15:44:37 -0300

    cambio en el script

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,13 @@ let precioBase = 400000;
 let valor = document.querySelector(".valorTotal");
 let mas = document.querySelector(".mas");
 let menos = document.querySelector(".menos");
-menos.onclick = function(){
-    cantidad.innerHTML--;
-    valor.innerHTML=(cantidad.innerHTML * precioBase).toLocaleString("es-CL");
-}
+
 mas.onclick = function(){
     cantidad.innerHTML++;
+    valor.innerHTML=(cantidad.innerHTML * precioBase).toLocaleString("es-CL");
+}
+
+menos.onclick = function(){
+    cantidad.innerHTML--;
     valor.innerHTML=(cantidad.innerHTML * precioBase).toLocaleString("es-CL");
 }
